6fd3bc7f290dc5ce36acbdc89779587310a6cd35
Hisatoshi Tabuchi <tab.hsts@gmail.com>
2019-06-27 23:06:46 +0900

    勉強会に参加人数を表示させる機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_124622) do
+ActiveRecord::Schema.define(version: 2019_06_27_063054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_06_26_124622) do
     t.datetime "event_at"
     t.text "location"
     t.integer "price"
-    t.integer "capacity"
+    t.integer "capacity", default: 0, null: false
     t.string "event_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
