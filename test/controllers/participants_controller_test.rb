af5994148e406419a2bf9aa8a63abcd96ef56b59
Hisatoshi Tabuchi <tab.hsts@gmail.com>
2019-06-28 13:44:11 +0900

    fix:rubocopのコーディング規約に則るよう修正

--- test/controllers/participants_controller_test.rb
+++ test/controllers/participants_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class ParticipantsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
+  test 'should get create' do
     get participants_create_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     get participants_destroy_url
     assert_response :success
   end
-
 end
