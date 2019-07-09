4ce0a90a7aefda1fa0ada0b474dd7a291d3a77f6
Hisatoshi Tabuchi <tab.hsts@gmail.com>
2019-07-10 01:25:48 +0900

    プロフィールを編集できる機能を実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def build_resource(hash = {})
     hash[:uid] = User.create_unique_string
     super
+  end
+
+  potected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
