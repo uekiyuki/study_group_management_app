d6c6f970c98f684555c6854bab4c61cb90526c3c
Hisatoshi Tabuchi <tab.hsts@gmail.com>
2019-07-09 19:42:14 +0900

    fix:勉強会に参加する機能をAjax対応するよう修正

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,22 +1,16 @@
 class ParticipantsController < ApplicationController
   before_action :authenticate_user!
+  respond_to? :js
+
   def create
     @event = Event.find(params[:event_id])
-    if @event.participants.count >= @event.capacity
-      redirect_to event_path(@event), notice: '定員オーバーのため参加できません'
-    elsif current_user.participants.create(event_id: @event.id)
-      redirect_to event_path(@event), notice: '勉強会の参加登録が完了しました'
-    else
-      redirect_to event_path(@event), notice: '勉強会の参加登録ができませんでした。開催者にコメントをお願いします。'
-    end
+    redirect_to event_path(@event), notice: '定員オーバーのため参加できません' if @event.participants.count >= @event.capacity
+    current_user.participants.create(event_id: @event.id)
   end
 
   def destroy
     @participant = Participant.find(params[:id])
-    if @participant.destroy
-      redirect_to event_path(@participant.event_id), notice: '勉強会の参加をキャンセルしました'
-    else
-      redirect_to event_path(@participant.event_id), notice: '勉強会の参加をキャンセルできませんでした'
-    end
+    @event = @participant.event
+    @participant.destroy
   end
 end
