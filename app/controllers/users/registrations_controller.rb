187040c2d484bff8bdf5eea11b03488bc676533e
田渕久敏 <tab.hsts@gmail.com>
2019-07-13 11:02:27 +0900

    add:レイアウトの追加修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,5 +10,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
