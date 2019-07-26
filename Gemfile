d1e188cd7683eb6140730071f4ab986e528c3390
田渕久敏 <tab.hsts@gmail.com>
2019-07-27 02:30:17 +0900

    N+1問題対策

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'letter_opener_web'
+  gem 'bullet'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,12 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
+
+  config.after_initialize do
+    Bullet.enable = true # Bulletプラグインを有効
+    Bullet.alert = true # JavaScriptでの通知
+    Bullet.bullet_logger = true # log/bullet.logへの出力
+    Bullet.console = true # ブラウザのコンソールログに記録
+    Bullet.rails_logger = true # Railsログに出力
+  end
 end
