7a96602a6fc4fe59228ba733fc03a051531d7a22
田渕久敏 <tab.hsts@gmail.com>
2019-08-04 14:34:33 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-25.times do |n|
-  title = 'test_title' + n.to_s
-  content = 'test_content' + n.to_s
-  user_id = 3
-  Event.create!(title: title, content: content, user_id: user_id)
+# テストユーザを作成
+  name = 'test user'
+  email = 'tabuchi@dic.com'
+  password = 'tabuchi'
+  password_confirmation = 'tabuchi'
+  User.create!(name: name, email: email, password: password, password_confirmation: password_confirmation)
+
+# イベントを作成
+10.times do |n|
+  title = 'test title ' + n.to_s
+  content = 'test content ' + n.to_s
+  location = 'somewhere in Tokyo ' + n.to_s
+  price = 1_000
+  event_at = '2019-10-01'
+  capacity = 5
+  user_id = User.find_by(email: 'tabuchi@dic.com').id
+
+  Event.create!(title: title,
+                content: content,
+                location: location,
+                price: price,
+                event_at: event_at,
+                capacity: capacity,
+                user_id: user_id)
 end
 
+# タグを作成
 1.times do |n|
-  @event = Event.find(1)
-  @event.tag_list.add("参加費無料", "期間限定","女子限定","２０代限定","３０代限定")
-  @event.save
+  event = Event.find(1)
+  event.tag_list.add("参加費無料", "期間限定","女子限定","２０代限定","３０代限定")
+  event.save
 end
