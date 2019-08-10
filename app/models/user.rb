92001acb8585393e4ae4588f05d468d27e8c7e5b
田渕久敏 <tab.hsts@gmail.com>
2019-08-11 00:24:13 +0900

    fix:バリデーションのバグとレイアウト変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 16 }
+
   # Include default devise modules. Others available are:
   # :confirmable :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
